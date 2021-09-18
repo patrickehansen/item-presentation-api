4d74720f66a4996c564c4e4dcf275771429cda47
Tiskahar <tiskahar@adiclan.com>
2021-09-18 12:04:25 -0500

    add environment variables, trying to get lambda export working

--- src/config.ts
+++ src/config.ts
@@ -4,4 +4,5 @@ dotenv.config();
 
 export default {
   region: process.env.REGION,
+  env: process.env.ENV,
 }
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import { createServer } from './server';
+import config from './config';
+export * from './lambda';
 
-createServer().then((server) => {
-  server.listen(3000, () => {
-    console.info('Server listening on http://localhost:3000')
+if (config.env === 'local') {
+  createServer().then((server) => {
+    server.listen(3000, () => {
+      console.info('Server listening on http://localhost:3000')
+    })
+  }).catch(err => {
+    console.error(`Error: ${err}`)
   })
-}).catch(err => {
-  console.error(`Error: ${err}`)
-})
+}
